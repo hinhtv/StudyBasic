a1f5ecf6dc26e747073ed371bb36dd4b730a1f0b
Hinh <v.hinhtv3@vinid.net>
2020-04-03 23:19:16 +0700

    Functional Programming

---
+++ Functional Programming/Refactor Global Variables Out of Functions.js
@@ -0,0 +1,42 @@
+/**
+So far, we have seen two distinct principles for functional programming:
+
+1) Don't alter a variable or object - create new variables and objects and return them if need be from a function.
+
+2) Declare function arguments - any computation inside a function depends only on the arguments, and not on any global object or variable.
+
+Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.
+
+Rewrite the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of the array passed to it and return a new array (list). The remove function should remove the given bookName from the array passed to it.
+
+Note: Both functions should return an array, and any new parameters should be added before the bookName parameter.
+ */
+
+ // ANSWER
+
+// The global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add (bookList, bookName) {
+
+  let newBookList = [...bookList, bookName];
+  return newBookList;
+
+  // Change code above this line
+}
+
+// Change code below this line
+function remove (bookList, bookName) {
+  if (bookList.indexOf(bookName) >= 0) {
+    let index = bookList.indexOf(bookName);
+    return bookList.slice(0, index).concat(bookList.slice(index + 1));
+    // Change code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
